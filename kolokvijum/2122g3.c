c6a89164842321d96d58187248b9f513716f4f63
1tsn0tf1l1p <fcobanin@gmail.com>
2023-12-13 10:04:06 +0100

    .

--- kolokvijum/2122g3.c
+++ kolokvijum/2122g3.c
@@ -10,21 +10,25 @@ double rek(int n, int temp, int polovina, int i)
 
     if (i < polovina)
     {
-        printf("n=%d, temp=%d, polovina=%d, i=%d\n", n, temp, polovina, i);
+        /// printf("n=%d, temp=%d, polovina=%d, i=%d\n", n, temp, polovina, i);
         return sqrt(1.0 * n / pow(i, 2) + rek(n - 1, temp, polovina, i + 1));
     }
     else if (temp % 2 == 0 && i == polovina)
     {
-        printf("n=%d, temp=%d, polovina=%d, i=%d\n", n, temp, polovina, i);
+        /// printf("n=%d, temp=%d, polovina=%d, i=%d\n", n, temp, polovina, i);
         return sqrt(1.0 * polovina / pow(i, 2) + rek(n + 2, temp, polovina, i + 1));
     }
     else
     {
-        printf("n=%d, temp=%d, polovina=%d, i=%d\n", n, temp, polovina, i);
+        /// printf("n=%d, temp=%d, polovina=%d, i=%d\n", n, temp, polovina, i);
         return sqrt(1.0 * n / pow(i, 2) + rek(n + 1, temp, polovina, i + 1));
     }
 
-    printf("n=%d, temp=%d, polovina=%d, i=%d\n", n, temp, polovina, i);
+    /// printf("n=%d, temp=%d, polovina=%d, i=%d\n", n, temp, polovina, i);
+}
+
+double iterativno(int n)
+{
 }
 
 int main()
@@ -34,6 +38,6 @@ int main()
     scanf("%d", &n);
 
     printf("%lf\n", rek(n, n, n / 2 + 1, 1));
-
+    printf("%lf\n", iterativno(n));
     return 0;
 }
